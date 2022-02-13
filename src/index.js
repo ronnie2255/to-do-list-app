a4d8c0e0f33827894640b6ea7edaaf72deb4be13
rohan <ronnie2255@icloud.com>
2022-02-13 10:49:59 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+
+const DATA = [
+  {id: "todo-0", name: "Eat", completed: true},
+  {id: "todo-1", name: "Code", completed: false},
+  {id: "todo-2", name: "Sleep", completed: false}
+]
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App tasks={DATA} />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+
+
